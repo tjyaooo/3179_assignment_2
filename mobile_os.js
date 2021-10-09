92025dc612ae82fc30ef03ec3a97a5f70017eade
tjyaooo <jtii0001@student.monash.edu>
2021-10-10 01:49:16 +0800

    line plot

--- mobile_os.js
+++ mobile_os.js
@@ -1,4 +1,9 @@
 var vg_1 = "mobile_os.vg.json";
+var vg_2 = "desktop_browser.vg.json";
 vegaEmbed("#world_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#desktop_market_share", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
