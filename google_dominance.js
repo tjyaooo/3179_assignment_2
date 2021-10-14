32b3d0702af46fa841f7f6475fdf17a84d36fc1c
tjyaooo <jtii0001@student.monash.edu>
2021-10-14 16:31:44 +0800

    add donut chart

--- google_dominance.js
+++ google_dominance.js
@@ -1,5 +1,6 @@
 var vg_1 = "mobile_os.vg.json";
 var vg_2 = "desktop_browser.vg.json";
+var vg_3 = "search_engine_share.vg.json";
 vegaEmbed("#world_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -7,3 +8,7 @@ vegaEmbed("#world_map", vg_1).then(function(result) {
 vegaEmbed("#desktop_market_share", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+vegaEmbed("#search_engine_share", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
