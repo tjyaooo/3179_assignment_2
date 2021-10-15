f9390921e8f011ae0d730fd4502c2c0f7db88473
tjyaooo <jtii0001@student.monash.edu>
2021-10-15 12:32:22 +0800

    add waterfall chart

--- google_dominance.js
+++ google_dominance.js
@@ -1,6 +1,7 @@
 var vg_1 = "mobile_os.vg.json";
 var vg_2 = "desktop_browser.vg.json";
 var vg_3 = "search_engine_share.vg.json";
+var vg_4 = "yearly_revenue.vg.json";
 vegaEmbed("#world_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -10,5 +11,9 @@ vegaEmbed("#desktop_market_share", vg_2).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#search_engine_share", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+vegaEmbed("#yearly_revenue_graph", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
